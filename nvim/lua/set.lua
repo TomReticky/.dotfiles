31c7e5c79ef49d1d63587ab6bcc8a933a2a7337c
TomReticky <tomas.reticky@gmail.com>
2022-12-17 12:44:44 +0100

    sixth commit

--- nvim/lua/set.lua
+++ nvim/lua/set.lua
@@ -7,10 +7,16 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.hlsearch = true
+
+vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+
 vim.g.mapleader = " "
